43b192671559c8d43f3a84462636aea933acf145
Louise Song <louise.s.song@gmail.com>
2025-05-22 17:01:10 -0700

    more cookies

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,20 @@ const CountDisplayer = ({count}) => {
 	)
 }
 
+const CookieImage = () => (
+	<img src = "https://images.ricardocuisine.com/services/recipes/1x1/443664758542a9fa22f234.jpg" width="100px" height="100px" />
+);
+
+const MoreCookies = () => (
+	<button>
+		<CookieImage></CookieImage>
+	<h1> "Don't click!"</h1>
+	</button>
+);
 const Cookie = ({onClick}) => {
 	return (
 		<button onClick={onClick}>
-			<img src = "https://images.ricardocuisine.com/services/recipes/1x1/443664758542a9fa22f234.jpg" width="100px" height="100px" />
+			<CookieImage></CookieImage>
 			<h1> Click Me!</h1>
 		</button>
 	)
@@ -19,10 +29,16 @@ const Cookie = ({onClick}) => {
 
 export default function App() {
 	const [currentCount, setCurrentCount] = useState(0);
+
+	const cookies = [];
+	for (let i = 0; i < currentCount; i++){
+		cookies.push(<MoreCookies key={i} left={100 + i*100} />);
+	}
 	return (
 		<div>
 			<CountDisplayer count={currentCount}/>
 			<Cookie onClick={() => setCurrentCount(currentCount + 1) }/>
+			{cookies}
 		</div>
         );
 
